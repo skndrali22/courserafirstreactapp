56e3e8d895b03fc3cfc6f91dcb9ca7f4911a42ab
skndrali22 <skndrshaikh17c@gmail.com>
2024-09-19 01:04:47 +0500

    Dynamic events Lab Coursera

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 // import logo from './logo.svg';
 import './App.css';
-import Cards from './components/Cards';
+// import Cards from './components/Cards';
 
 
 function App() {
+
+  function handleClick(){
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNum);
+    let userInput = prompt('Guess a number'); 
+    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+  }
+
   return (
-    <div className="App">
-        <h1>Task: Add three Card elements</h1>
-        <Cards h2="First card's h2" h3="First card's h3" />
-        <Cards h2="Second card's h2" h3="Second card's h3" />
-        <Cards h2="Third card's h2" h3="Third card's h3" />
+    // <div className="App">
+    //     <h1>Task: Add three Card elements</h1>
+    //     <Cards h2="First card's h2" h3="First card's h3" />
+    //     <Cards h2="Second card's h2" h3="Second card's h3" />
+    //     <Cards h2="Third card's h2" h3="Third card's h3" />
+    // </div>
+
+    <div>
+      <h1>Task: Add a button and handle a click event</h1>
+
+      <button 
+        onClick = {handleClick}>
+        Guess the number between 1 and 3
+      </button>
     </div>
+
+
   );
 }
 
